faa58b3755bf86143cc0a12e954689890a7b0b03
Andray <robot-tnt@yandex.ru>
2023-09-17 20:07:07 +0300

    Домашнее задание 6.1

--- DZ_06/chess/queen_8.py
+++ DZ_06/chess/queen_8.py
@@ -1,44 +1,42 @@
 import random
 
+def queen_attack (queens_pos: list[list], position: list[int]) -> bool:
+    for queen in queens_pos:
+        if(queen[0] - position[0]) == (position[1] - queen[1]) or\
+        (queen[0] - position[1]) == (queen[1] - position[0]) or\
+        queen[0] == position[0] or\
+        queen[1] == position[1]:
+            return False
+    return True
+
 # Расстановка ферзей
 def queens ():
     queens_position = []
-    position = []
+    position = [0, 0]
     row = [0, 1, 2, 3, 4, 5, 6, 7]
     column = [0, 1, 2, 3, 4, 5, 6, 7]
+    stat = True
     
-    while row:
-        position = []
-        position.append(random.choice(row))
+    position[0] = random.choice(row)
+    position[1] = random.choice(column)
+    row.remove(position[0])
+    column.remove(position[1])
+    queens_position.append(position)
+    print(queens_position)
+    
+    while len(row):
+        position[0] = random.choice(row)
         row.remove(position[0])
-        
-        if len(queens_position) != 0:
-            while True:
-                count = 0
-                for queen in queens_position:
-                    position.append(random.choice(column))
-                    # print(queen, position)
-                    if (queen[0] - position[0]) == (position[1] - queen[1]) or\
-                    (queen[0] - position[1]) == (queen[1] - position[0]) or\
-                    queen[0] == position[0] or\
-                    queen[1] == position[1]:
-                        position.pop()
-                        count += 1
-                
-                if count == 0: 
-                    queens_position.append(position)
-                    column.remove(position[1])
-                    position = []
-                    break
-                break
-
-        else:        
-            position.append(random.choice(column))
-            column.remove(position[1])
-            queens_position.append(position)
-            position = []
-            
-        
+        stat = True
+        while stat:
+            # print(position)
+            position[1] = random.choice(column)
+            print(queen_attack(queens_position, position))
+            if queen_attack(queens_position, position):
+                stat = False 
+        queens_position.append(position)
+        column.remove(position[1])
+        print(position)       
     return queens_position
 
 print(queens())
