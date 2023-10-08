ca8d053e4756901519d3b2d75ae4a2c2ef0d5ee6
Andray <robot-tnt@yandex.ru>
2023-10-08 11:13:50 +0300

    Домашнее задание 10

--- DZ_10/DZ_10_2.py
+++ DZ_10/DZ_10_2.py
@@ -1,4 +1,10 @@
-from DZ_10_1 import Dog
+# Класс принимает тип животного (название одного из созданных классов) 
+# и параметры для этого типа.
+# Внутри класса создайте экземпляр на основе переданного типа 
+# и верните его из класса-фабрики.
+
+
+from DZ_10_1 import *
 
 # pet = Dog('Bob', 5, 'Скулит')
 # print(pet.name, pet.spec)
@@ -6,8 +12,14 @@ from DZ_10_1 import Dog
 class Fabrica(Dog):
     
     def __init__(self, name, age, spec):
-        super().__init__(name, age, spec)
+        super.__init__(name, age, spec)
+        self.name = name
+        self.age = age
+        self.gav = spec
     
-    bobic = Dog("Тузик", 3, "Воет")
-    
-print(Fabrica.bobic.name, Fabrica.bobic.spec)
+    p1 = Dog("Тузик", 5, "Воет")
+    # p1.speed = 25
+
+
+Fabrica.p1.speed = 25
+print(Fabrica.p1.name, Fabrica.p1.spec, Fabrica.p1.speed)
